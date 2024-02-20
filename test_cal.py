6fef84c335a6605b7452ff0b2bea2d0aca514a43
Unthreaded1901 <108473178+Unthreaded1901@users.noreply.github.com>
2024-02-20 11:55:31 +0530

    Create test_cal.py

---
+++ test_cal.py
@@ -0,0 +1,13 @@
+from Calculator import*
+
+def test_add():
+  assert add(5,2) == 7
+
+def test_subtract():
+  assert subtract(7,4) == 3
+
+def test_multiply():
+  assert multiply(3,2) == 6
+
+def test_divide():
+  assert divide(9,3) == 3
